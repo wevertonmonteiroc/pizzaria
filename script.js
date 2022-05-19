01c13b4a744ba8b899a22a922fe89352aeee8468
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-05-19 14:00:18 -0300

    Exibição das informações na tela através da função map, e criação das funções para selecionar os elementos com querySelector e querySelectoAll

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log(pizzaJson);
+// Criação das funções para seleção de elementos
+const c = (el)=> document.querySelector(el);
+const cs = (el))=> document.querySelectorAll(el);
+
+pizzaJson.map((item, index)=>{
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    // Preencher as informações em pizzaitem
+
+    c('.pizza-area').append(pizzaItem);
+});
