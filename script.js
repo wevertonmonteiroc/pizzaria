848955011d8f82454942809ed6a5883e92bbbffd
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-05-19 14:41:06 -0300

    Adicionei as informações das pizzas: nome,descrição, preço e imagem

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-// Criação das funções para seleção de elementos
 const c = (el)=> document.querySelector(el);
-const cs = (el))=> document.querySelectorAll(el);
+const cs = (el)=> document.querySelectorAll(el);
 
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
-    // Preencher as informações em pizzaitem
-
+    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+      
+    
     c('.pizza-area').append(pizzaItem);
 });
