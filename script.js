5c3bbd1259e31f630845b765875e8060d81eae4a
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-05-22 22:33:41 -0300

    Nessa modificação alterei o tratamento do carrinho de compras: aparecer quando tiver produtos no carrinho, e mostrar as informações dos produtos adicionados

--- script.js
+++ script.js
@@ -72,12 +72,8 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
 })
 c('.pizzaInfo--addButton').addEventListener('click', ()=>{
     let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
-
-
     let identifier = pizzaJson[modalKey].id+'@'+size;
-
     let key = cart.findIndex((item)=>item.identifier == identifier);
-
     if(key > -1){
             cart[key].qt += modalQt;
     } else {
@@ -88,8 +84,20 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
             qt:modalQt
         });
     }
-    
-
-
+    updateCart();  
     closeModal();
 });
+
+function updateCart() {
+    if(cart.length > 0) {
+        c('aside').classList.add('show');
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+
+            console.log(pizzaItem);
+        }
+        
+    } else {
+        c('aside').classList.remove('show');
+    }
+}
