7eecfdec9ca3546e1f9832cdc4815b8a44935c28
Weverton Monteiro <wevertonmonteiroc@gmail.com>
2022-05-22 21:51:58 -0300

    Nessas alterações, realizei modificações relacionadas ao carrinho de compras: adicionar o id, tamanho e quantidade das pizzas no carrinho, e fechar o modal após adição.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el)=> document.querySelector(el);
 const cs = (el)=> document.querySelectorAll(el);
@@ -17,6 +19,7 @@ pizzaJson.map((item, index)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -67,3 +70,14 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
         size.classList.add('selected');
     });
 })
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    cart.push({
+        id:pizzaJson[modalKey].id,
+        size,
+        qt:modalQt
+    });
+
+    closeModal();
+});
